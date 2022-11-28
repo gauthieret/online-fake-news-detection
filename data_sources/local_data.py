d003f9ca610b0d1b7609457bd92843f0f7ea3c8a
Gauthier Etien <gauthier.etien@gmail.com>
2022-11-28 17:34:45 +0100

    adapt file loading

--- data_sources/local_data.py
+++ data_sources/local_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
 def getdata(data):
-    data = f"../online-fake-news-detection/raw_data/{data}.csv"
+    data = f"../online-fake-news-detection/data/{data}.csv"
     data_df = pd.read_csv(data)
     return data_df
