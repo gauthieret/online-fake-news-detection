86292f0263c7f73905cca08a0d964004f37ecf2b
Dinis Timoteo <115228493+DinisAT@users.noreply.github.com>
2022-11-28 15:32:43 +0100

    minor styling issues

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -5,20 +5,20 @@ import numpy as np
 
 def train_model(X: np.ndarray):
 
-    tfidf_vectorizer=TfidfVectorizer(max_df=0.7)
-    tfidf=tfidf_vectorizer.fit(X)
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.7)
+    tfidf = tfidf_vectorizer.fit(X)
     return tfidf
 
 def transform_model(X: np.ndarray):
 
-    tfidf_vectorizer=TfidfVectorizer(max_df=0.7)
-    tfidf=tfidf_vectorizer.transform(X)
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.7)
+    tfidf = tfidf_vectorizer.transform(X)
 
     return tfidf
 
 def pa_classifier_fit(tfidf_train, y_train):
 
-    pac=PassiveAggressiveClassifier(max_iter=50, n_jobs = -1, random_state=0, fit_intercept=False, early_stopping=True,
+    pac=PassiveAggressiveClassifier(max_iter=50, n_jobs=-1, random_state=0, fit_intercept=False, early_stopping=True,
                                 validation_fraction=0.2, n_iter_no_change=5)
     pac.fit(tfidf_train,y_train)
 
@@ -26,6 +26,6 @@ def pa_classifier_fit(tfidf_train, y_train):
 
 def y_pred(pac, tfidf_test):
 
-    y_pred=pac.predict(tfidf_test)
+    y_pred = pac.predict(tfidf_test)
 
     return y_pred
