16b466754a991231b0c4f846df68419bc9f5a418
Dinis Timoteo <115228493+DinisAT@users.noreply.github.com>
2022-11-30 12:17:27 +0100

    saved changes

--- interface/main.py
+++ interface/main.py
@@ -1,4 +1,5 @@
 from ml_logic.preprocessor import *
+from ml_logic.encoders import *
 from ml_logic.params import TARGET_COLUMN
 
 
@@ -7,24 +8,24 @@ def split_data(raw_data):
 
     selected_columns = preparation(raw_data)
 
-    # cleaned_columns =  selected_columns.apply(clean)
-
     X, y = X_y(selected_columns, TARGET_COLUMN)
 
     X_train, X_test, y_train, y_test = split_data(X, y)
 
-    # X_train_vect = train_vect(X_train)
-    # X_train_trans = transform_vect(X_train_vect)
-
-    # X_test_vect = train_vect(X_test)
-
     return X_train, X_test, y_train, y_test
 
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.pipeline import Pipeline
+def train(X, y):
 
-cleaner = FunctionTransformer(clean)
+    pipeline.fit(X, y)
 
-preprocessor = Pipeline([
-    ('cleaner', cleaner)
-                         ])
+    return pipeline
+
+def predict(X_test):
+
+    pipeline.predict(X_train, y_train)
+
+    return model_trained
+
+def predict(new_data):
+
+     = pipe.predict(new_data)
