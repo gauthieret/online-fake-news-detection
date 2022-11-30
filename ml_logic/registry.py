501b8b281328e68c79b5f318ed363854001ff47d
Dinis Timoteo <115228493+DinisAT@users.noreply.github.com>
2022-11-30 16:47:00 +0100

    train, predict, save and load functions tested and working

--- ml_logic/registry.py
+++ ml_logic/registry.py
@@ -4,13 +4,15 @@ import os
 import glob
 from joblib import dump, load
 
+LOCAL_REGISTRY_PATH = '..'
+
 def save_model (model = None):
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
 
     model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", timestamp)
     #model.save(model_path)
-    dump(model, LOCAL_REGISTRY_PATH)
+    dump(model, model_path)
 
     return None
 
@@ -20,9 +22,11 @@ def load_model():
 
 
     results = glob.glob(f"{model_directory}/*")
+    if not results:
+        return None
+
     model_path = sorted(results)[-1]
 
     #model = models.load_model(model_path)
     model = load(model_path)
-
     return model
