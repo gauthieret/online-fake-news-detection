2a09a07e0e1aca1db9cca3e95be84dca751dddef
Gauthier Etien <gauthier.etien@gmail.com>
2022-12-05 13:51:53 +0100

    modify the labels and error message for the predictions

--- GEofnd/interface/main.py
+++ GEofnd/interface/main.py
@@ -3,12 +3,13 @@ from GEofnd.ml_logic.params import TARGET_COLUMN, FEATURE_COLUMN
 from GEofnd.ml_logic.registry import save_model, load_model
 from GEofnd.ml_logic.encoders import MNB, CountVect, pipe
 from GEofnd.ml_logic.scraping_module import scraping
+from GEofnd.ml_logic.classifier import result
+
 import pandas as pd
 
 
 df_dataset = getdata()
-url = 'https://edition.cnn.com/europe/live-news/russia-ukraine-war-news-11-29-22/index.html'
-#url = 'https://www.bloomberg.com/news/articles/2022-12-01/kim-kardashian-s-investment-firm-hires-brisske-from-permira'
+url = "https://ec.europa.eu/commission/presscorner/detail/en/IP_22_7406"
 
 def prep_split_data(df=None):
 
@@ -31,60 +32,38 @@ def train(X = None, y = None):
 
 def predict(url):
 
+    #if its a website
     if url.startswith('http') or url.startswith('www'):
         model = load_model()
 
         url_df = scraping(url)
-        url_df['cleaned_news'] = url_df['news'].apply(clean)
+        if url_df['news'][0] == None:
+            return 'the bat seems tired today, please try by copy pasting the text of your article'
 
+        #if the scraping does not work
+        else :
+            url_df['cleaned_news'] = url_df['news'].apply(clean)
 
+            prediction = model.predict(url_df['cleaned_news'])
+            predict_proba = model.predict_proba(url_df['cleaned_news'])
 
-        prediction = model.predict(url_df['cleaned_news'])
-        predict_proba = model.predict_proba(url_df['cleaned_news'])
+            label = result(prediction,predict_proba)
+            return label
 
-        if prediction[0] == True:
-            if predict_proba[0][1] > 0.70:
-                print("It's true!")
-            elif predict_proba[0][1] > 0.5:
-                print('Probably true')
-            elif predict_proba[0][1] <= 0.5:
-                print('possibly true dude')
-
-        elif prediction[0] == False:
-            if predict_proba[0][0] > 0.70:
-                print("It's a fake news")
-            elif predict_proba[0][0] > 0.5:
-                print('Probably fake')
-            elif predict_proba[0][1] <= 0.5:
-                print('Possibly fake')
-
+    #if it is a text
     else :
 
         model = load_model()
+
         url_df = pd.DataFrame({'news': [url]})
-
         url_df['cleaned_news'] = url_df['news'].apply(clean)
-
-
 
         prediction = model.predict(url_df['cleaned_news'])
         predict_proba = model.predict_proba(url_df['cleaned_news'])
 
-        if prediction[0] == True:
-            if predict_proba[0][1] > 0.70:
-                print("It's true!")
-            elif predict_proba[0][1] > 0.5:
-                print('Probably true')
-            elif predict_proba[0][1] <= 0.5:
-                print('possibly true dude')
+        label = result(prediction,predict_proba)
 
-        elif prediction[0] == False:
-            if predict_proba[0][0] > 0.70:
-                print("It's a fake news")
-            elif predict_proba[0][0] > 0.5:
-                print('Probably fake')
-            elif predict_proba[0][1] <= 0.5:
-                print('Possibly fake')
+        return label
 
 
 if __name__ == '__main__':
--- GEofnd/ml_logic/classifier.py
+++ GEofnd/ml_logic/classifier.py
@@ -4,16 +4,20 @@ def result(prediction, predict_proba):
 
     if prediction[0] == True:
         if predict_proba[0][1] > 0.70:
-            return "It's true!"
+            return f"It's true! Probability of {round(predict_proba[0][1]*100,2)}%"
         elif predict_proba[0][1] > 0.5:
-            return 'Probably true'
+            return f'Probably true, probability of {round(predict_proba[0][1]*100,2)}%'
+        elif predict_proba[0][1] <= 0.25:
+            return f'Seems to be true but lack of info on the subject'
         elif predict_proba[0][1] <= 0.5:
-            return 'possibly true dude'
+            return f'possibly true dude but probability of {round(predict_proba[0][1]*100,2)}%'
 
     elif prediction[0] == False:
         if predict_proba[0][0] > 0.70:
-            return "It's a fake news"
+            return f"It's a fake news with a probability of {round(predict_proba[0][1]*100,2)}%"
         elif predict_proba[0][0] > 0.5:
-            return 'Probably fake'
+            return f'Probably fake with a probability of {round(predict_proba[0][1]*100,2)}%'
+        elif predict_proba[0][1] <= 0.25:
+            return f'Seems to be fake but lack of info on the subject'
         elif predict_proba[0][1] <= 0.5:
-            return 'Possibly fake'
+            return f'Possibly fake but probability of {round(predict_proba[0][1]*100,2)}%'
